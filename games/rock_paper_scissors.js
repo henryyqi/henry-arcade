46260901a7f364b7a678d4f51c01018cc22b5a06
Henry <71992845+henryyqi@users.noreply.github.com>
2025-10-13 17:20:18 -0700

    Added secret milestone message for 100 wins

--- games/rock_paper_scissors.js
+++ games/rock_paper_scissors.js
@@ -6,13 +6,14 @@
 5. play again?
 */
 
-// testing
+// button event listeners
 document.getElementById('rock').addEventListener('click', () => {playGame('rock'); updateUserChoiceDisplay('rock');});
 document.getElementById('paper').addEventListener('click', () => {playGame('paper'); updateUserChoiceDisplay('paper');});
 document.getElementById('scissors').addEventListener('click', () => {playGame('scissors'); updateUserChoiceDisplay('scissors');});
 
 const resultDiv = document.getElementById('result');
 const scoreboardDiv = document.getElementById('scoreboard');
+const secretDisplayDiv = document.getElementById('secret-display');
 
 const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', resetScores);
@@ -22,9 +23,7 @@ function updateUserChoiceDisplay(userChoice) {
     userChoiceDiv.innerHTML = `YOU CHOSE<br><img src="./rps_pics/${userChoice}_icon.png" alt="${userChoice.charAt(0).toUpperCase()} Icon" height="144" width="144" class="icon">`;
 }
 
-// elements
 const computerChoiceDiv = document.getElementById('computer-choice');
-
 function updateComputerChoiceDisplay(computerChoice) {
     computerChoiceDiv.innerHTML = `COMPUTER CHOSE<br><img src="./rps_pics/${computerChoice}_icon.png" alt="${computerChoice.charAt(0).toUpperCase()} Icon" height="144" width="144" class="icon">`;
 }
@@ -73,9 +72,20 @@ function resetScores() {
     displayResult("Scores have been reset.");
 }
 
+function checkHundredWins() {
+    if (scores.user >= 100 && scores.computer <100) {
+        secretDisplayDiv.innerHTML = `CONGRATS! YOU REACHED 100 WINS! Don't you have something better to do?`;
+    } else if (scores.computer >= 100 && scores.user <100) {
+        secretDisplayDiv.innerHTML = `OH NO! THE COMPUTER REACHED 100 WINS BEFORE YOU! It's time to take a break...`;
+    } else {
+        secretDisplayDiv.innerHTML = `<br>`;
+    }
+}
+
 function playGame(userChoice) {
     const computerChoice = getComputerChoice();
     const result = determineWinner(userChoice, computerChoice);
     displayResult(`${result}`);
     updateScoreboard();
+    checkHundredWins();
 }

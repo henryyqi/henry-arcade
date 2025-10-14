d0be6cf6771317257fb2cc6fc7406819665f11dc
Henry <71992845+henryyqi@users.noreply.github.com>
2025-10-13 18:30:59 -0700

    Added link to html from main game mmenu buttons

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-document.getElementById('rock_paper_scissors_game').addEventListener('click', () => {
-    window.location.href = 'games/rock_paper_scissors.html';
-});
+// document.getElementById('rock_paper_scissors_game').addEventListener('click', () => {
+//     window.location.href = 'games/rock_paper_scissors.html';
+// });
 
-document.getElementById('tic_tac_toe_game').addEventListener('click', () => {
-    window.location.href = 'games/tic_tac_toe.html';
-});
+// document.getElementById('tic_tac_toe_game').addEventListener('click', () => {
+//     window.location.href = 'games/tic_tac_toe.html';
+// });
 
-document.getElementById('connect_four_game').addEventListener('click', () => {
-    window.location.href = 'games/connect_four.html';
-});
+// document.getElementById('connect_four_game').addEventListener('click', () => {
+//     window.location.href = 'games/connect_four.html';
+// });
---
+++ tic_tac_toe.js
@@ -0,0 +1,13 @@
+/*
+
+1. randomly decide if player is X or O
+2. if player is X, computer is O, player goes first
+3. if player is O, computer is X, computer goes first
+4. if computer goes first, randomly place an X in an empty square
+5. wait for player to click an empty square
+6. computer randomly puts their piece in an empty square
+7. check for win/tie after every turn
+8. if win/tie, display message and reset board after a few seconds
+9. if no win/tie, continue until win/tie
+
+*/
